3cf2fbcda9a13830543f34fc7274843f2a775408
aditiyakamodiya <aditiyakamodiya@gmail.com>
2024-03-01 20:51:47 +0530

    cloud

--- Connection.js
+++ Connection.js
@@ -1,6 +1,7 @@
 import { MongoClient,ServerApiVersion } from "mongodb";
-const connection = MongoClient.connect('mongodb://127.0.0.1:27017')
-// mongodb+srv://adityakamodiya:11223344@cluster0.j4ukslx.mongodb.net/Mywebsite?retryWrites=true&w=majority
+const connection = MongoClient.connect(' mongodb+srv://adityakamodiya:11223344@cluster0.j4ukslx.mongodb.net/Mywebsite?retryWrites=true&w=majority')
+// mongodb://127.0.0.1:27017
+
 
 export const dbName = 'Mywebsite'
 // export const dbName = 'passwordbase'
