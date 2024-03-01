1473a716cddfbf7d3285786866e50a44d353e282
aditiyakamodiya <aditiyakamodiya@gmail.com>
2024-03-01 20:59:45 +0530

    clouddd

--- Connection.js
+++ Connection.js
@@ -1,6 +1,9 @@
 import { MongoClient,ServerApiVersion } from "mongodb";
-const connection = MongoClient.connect(' mongodb+srv://adityakamodiya:11223344@cluster0.j4ukslx.mongodb.net/Mywebsite?retryWrites=true&w=majority')
+const connection = MongoClient.connect(' mongodb+srv://adityakamodiya:11223344@cluster0.j4ukslx.mongodb.net/Mywebsite?retryWrites=true&w=majority&appName=Cluster0')
 // mongodb://127.0.0.1:27017
+
+
+// mongodb+srv://adityakamodiya:<password>@cluster0.j4ukslx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
 
 export const dbName = 'Mywebsite'
